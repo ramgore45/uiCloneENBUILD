33d86cc98c3f64e0464152001aa4e311df9882bf
ramgore45 <ramgore150@gmail.com>
2024-06-23 09:07:51 +0530

    changeinswidth

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
   return (
     <div  className={`${darkMode ? "dark " : ""}`}>
       <div className="flex h-fit relative">
-        <div className="w-[17%] h-screen fixed bg-white dark:bg-neutral-800 dark:text-neutral-300 transition duration-300">
+        <div className="min-w-[17%] h-screen fixed bg-white dark:bg-neutral-800 dark:text-neutral-300 transition duration-300">
           <Sidebar/>
         </div>
-        <div className="w-[83%] h-full absolute left-[17%]">
+        <div className="w-fit h-full absolute left-[17%]">
           <Navbar darkMode={darkMode}/>
           <Home toggle={toggle} darkMode={darkMode}/>
         </div>
